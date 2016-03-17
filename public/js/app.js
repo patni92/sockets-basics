638e53779a64b980a033166a578b37b175ae9af0
Patrik Nilsson <pn222en@student.lnu.se>
2016-03-17 19:53:33 +0100

    Add fron end form to send message

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,24 @@ var socket = io();
 
 socket.on("connect", function () {
     console.log("Conntected to socket.io server");
+
+
 });
+
+socket.on("message", function (message) {
+    console.log("New Message");
+    console.log(message.text);
+});
+
+var $form = jQuery("#message-form")
+
+$form.on("submit", function (event) {
+    event.preventDefault();
+    var message = $form.find("input[name=message]")
+
+    socket.emit("message", {
+        text: message.val()
+    })
+
+    message.val("");
+})
